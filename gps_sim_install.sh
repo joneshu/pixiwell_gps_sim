bd44b3782033058db8a48c2a1538121cacc4a163
joneshu <250516803@qq.com>
2023-03-19 13:54:59 +0800

    pixiwell_gps_sim

---
+++ gps_sim_install.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+echo "http://pixiwell.com"
+
+echo "Installing PIXIWELL_RF GPS_Sim"
+
+# sudo apt-get update -y --quiet
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
+# 	gcc \
+#     gnuradio \
+#     gr-osmosdr \
+#     hackrf \
+#     gqrx-sdr \
+#     libhackrf-dev \
+#     ;
+
+# echo "git clone "
+# git clone git@github.com:osqzss/gps-sdr-sim.git
+
+echo "gcc gps_sim "
+    
+gcc gpssim.c -lm -fopenmp -o gps-sdr-sim
+
+
